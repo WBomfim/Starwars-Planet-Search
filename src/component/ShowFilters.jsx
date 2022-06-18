0432b31237fcab1d41119631277fa0df9f590809
WBomfim <willianbomfim@hotmail.com>
2022-06-17 21:42:59 -0300

    Refactor: Refatorado o componente ShowFilters para renderizar na tela apenas quando tiver filtro ativo.

--- src/component/ShowFilters.jsx
+++ src/component/ShowFilters.jsx
@@ -9,40 +9,42 @@ function ShowFilters() {
   } = useContext(StarContext);
 
   return (
-    <section>
-      <div>
-        <h2>Filtros:</h2>
-      </div>
-      <div>
-        <ul>
-          {filterByNumericValues.map(({ column, comparison, value }, index) => (
-            index === filterByNumericValues.length - 1 ? null : (
-              <li
-                key={ index }
-                data-testid="filter"
-              >
-                <span>{`${column} ${comparison} ${value}`}</span>
-                <button
-                  type="button"
-                  id={ column }
-                  onClick={ (event) => removeFilter(event) }
+    filterByNumericValues.length > 1 && (
+      <section>
+        <div>
+          <h2>Filtros:</h2>
+        </div>
+        <div>
+          <ul>
+            {filterByNumericValues.map(({ column, comparison, value }, index) => (
+              index === filterByNumericValues.length - 1 ? null : (
+                <li
+                  key={ index }
+                  data-testid="filter"
                 >
-                  X
-                </button>
-              </li>
-            )))}
-        </ul>
-      </div>
-      <div>
-        <button
-          type="button"
-          data-testid="button-remove-filters"
-          onClick={ removeAllFilters }
-        >
-          Remover Filtros
-        </button>
-      </div>
-    </section>
+                  <span>{`${column} ${comparison} ${value}`}</span>
+                  <button
+                    type="button"
+                    id={ column }
+                    onClick={ (event) => removeFilter(event) }
+                  >
+                    X
+                  </button>
+                </li>
+              )))}
+          </ul>
+        </div>
+        <div>
+          <button
+            type="button"
+            data-testid="button-remove-filters"
+            onClick={ removeAllFilters }
+          >
+            Remover Filtros
+          </button>
+        </div>
+      </section>
+    )
   );
 }
 
